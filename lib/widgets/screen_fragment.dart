1030fc6ea94e744bc8d229ac8c0ec06a31a965f2
Dilan <dilan@dilan.me>
2021-09-12 15:22:18 +0530

    Incrementing version number to 4.0.17

--- lib/widgets/screen_fragment.dart
+++ lib/widgets/screen_fragment.dart
@@ -1,3 +1,8 @@
 import '../../imports.dart';
 
-abstract class ScreenFragment<T> extends ScreenMaster<T> {}
+abstract class ScreenFragment<T> extends ScreenMaster<T> {
+  @override
+  void onStart() {
+    super.onStart();
+  }
+}
